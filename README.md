98cc54a60a66fec309e26f80a2d58877b2cb43fe
鳥越幸樹 <niaoyuexingshu@gmail.com>
2025-07-02 14:51:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# template_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/todoreverpodapp/template_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.todoreverpodapp.template_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
